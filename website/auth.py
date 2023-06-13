138c0f1cf9422cb26fa4906f2a9d8dc6e5ef1e9b
jiwon <jiwon.=hur6@gmail.com>
2023-06-13 16:51:56 -0700

    testrun

--- website/auth.py
+++ website/auth.py
@@ -19,7 +19,7 @@ def sign_up():
         password2 = request.form.get('password2')
 
         if len(email) < 4:
-            flash('Email must be greater than 4 characters.', category='error')
+            flash('Email must be greater than 4 characters. // soon gonna change to username not email', category='error')
             pass
         elif len(firstName) < 2:
             flash('First name must be greater than 1 characters.', category='error')
