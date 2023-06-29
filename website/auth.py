8ad2113a31c4a3ec0023ac1f70f46fe0ce3579b2
jiwon <jiwon.=hur6@gmail.com>
2023-06-29 01:50:10 -0700

    addons

--- website/auth.py
+++ website/auth.py
@@ -1,36 +1,68 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash 
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password. Try again.', category='error')
+        else:
+            flash('Username does not exist.', category='error')
+
     return render_template("login.html", text="testing", boolean=True)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
-        email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        username = request.form.get('username')
+        first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
-            flash('Email must be greater than 4 characters. // soon gonna change to username not email back to username. it worked god is real', category='error')
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash('Username already exists. Please login.', category='error')
+        elif len(username) <= 2:
+            flash('Username must be greater than 2 characters.', category='error')
             pass
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First name must be greater than 1 characters.', category='error')
             pass
+        elif len(last_name) < 2:
+            flash('Last name must be greater than 1 characters.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
             pass
-        elif len(password1) < 7:
-            flash('Password must be at least 7 characters.', category='error')
+        elif len(password1) < 6:
+            flash('Password must be at least 6 characters.', category='error')
             pass
         else:
+            new_user = User(username=username, first_name=first_name, last_name=last_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
             flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
