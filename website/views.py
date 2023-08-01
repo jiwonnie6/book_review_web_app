d4d2bf87644c6ae6c493853364fa1cbd0408c43b
jiwon <jiwon.=hur6@gmail.com>
2023-07-31 19:21:05 -0700

    changes

--- website/views.py
+++ website/views.py
@@ -9,6 +9,22 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    # if request.method == 'POST':
+    #     note = request.form.get('note')
+        
+    #     if len(note) < 1:
+    #         flash('note is too short', category="error")
+    #     else:
+    #         new_note = Note(data=note, user_id=current_user.id)
+    #         db.session.add(new_note)
+    #         db.session.commit()
+    #         flash('note added', category="success ")
+
+    return render_template("home.html", user=current_user)
+
+@views.route('/book-review', methods=['GET', 'POST'])
+@login_required
+def book_review():
     if request.method == 'POST':
         note = request.form.get('note')
         
@@ -20,7 +36,7 @@ def home():
             db.session.commit()
             flash('note added', category="success ")
 
-    return render_template("home.html", user=current_user)
+    return render_template("book_review.html", user=current_user)
 
 @views.route('/delete-note', methods=['POST'])
 def delete_note():
